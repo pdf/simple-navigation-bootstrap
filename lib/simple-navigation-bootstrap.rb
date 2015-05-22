2c3e1a6ed89c04cf131e439c448b641d6ecc293c
Peter Fern <github@obfusc8.org>
2015-05-22 17:34:57 +1000

    Include split core extensions

--- lib/simple-navigation-bootstrap.rb
+++ lib/simple-navigation-bootstrap.rb
@@ -1,4 +1,5 @@
 require "simple-navigation"
+require "simple_navigation/core_ext/item_container.rb"
 require "simple_navigation/rendering/renderer/bootstrap"
 require "simple-navigation-bootstrap/engine"
 require "simple-navigation-bootstrap/version"
